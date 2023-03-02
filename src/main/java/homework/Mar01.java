05e43a8508f4304694d98b7c0e25c5420d65c5b9
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-03-02 10:01:25 +0700

    homework Mar 1st

---
+++ src/main/java/homework/Mar01.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class Mar01 {
+    /*
+    1. Read file employee.json from the resources folder
+    2. Create a class Employee with fields: id, name, age, city, jobTitle, salary
+    3. Create a method that returns a list of Employee objects from the json file
+    4. Create a method that returns a list of Employee objects that have a salary greater than 10000
+    5. Create a method that returns a list of Employee objects that have a salary greater than 10000 and live in San Francisco
+    6. Create a method that returns a list of Employee objects that have a salary less than 10000 and live in Boston or New York
+    7. Create a method that returns a list of Employee objects that live in Boston or New York and are older than 30
+    8. Sort the list of employees by salary in descending order
+    9. return the list of employees that have job title "Business Analyst"
+     */
+}
