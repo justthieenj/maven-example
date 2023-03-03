31ed2fbed41006e80a043d27f11d1a8870d57836
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-03-03 19:54:00 +0700

    add lombok

--- src/main/java/data/Dummy.java
+++ src/main/java/data/Dummy.java
@@ -1,51 +1,14 @@
 package data;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Dummy {
     private Integer id;
     private String title;
     private String description;
     private Double price;
     private List<String> images;
-
-    public List<String> getImages() {
-        return images;
-    }
-
-    public void setImages(List<String> images) {
-        this.images = images;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
--- src/main/java/data/Employee.java
+++ src/main/java/data/Employee.java
@@ -1,5 +1,8 @@
 package data;
 
+import lombok.Data;
+
+@Data
 public class Employee {
     private int id;
     private String name;
@@ -7,52 +10,4 @@ public class Employee {
     private String city;
     private String jobTitle;
     private Double salary;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
 }
