faa6411d8cbb65ec4afdcd9bb0dce9a539710ae0
Nguyen Lan <nchoanglan94@gmail.com>
2023-03-24 19:16:26 +0700

    Mar22 homework

--- src/main/java/homework/Mar22.java
+++ src/main/java/homework/Mar22.java
@@ -1,5 +1,13 @@
 package homework;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.Employee;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mar22 {
     /*
     1. Print all employees who have vehicle that registered before 2018
@@ -9,4 +17,45 @@ public class Mar22 {
     5. Use regex to check if employee email is valid or not (user@domain)
     6. Print all employees whose email is invalid
      */
+
+
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Employee> employeeList = new ArrayList<>(List.of(mapper.readValue(new File("src/test/resources/jsonfiles/employee.json"), Employee[].class)));
+
+        System.out.println("All employees who have vehicle that registered before 2018:");
+        for (Employee employee : employeeList) {
+            if (Integer.parseInt((String) employee.getVehicle().getRegistrationDate().subSequence(0, 4)) < 2018) {
+                System.out.println(employee.getName());
+            }
+        }
+
+        System.out.println("All employees who have vehicle that have registration date in the third quarter:");
+        for (Employee employee : employeeList) {
+            if (employee.getVehicle().getRegistrationDate().matches("\\d{4}/0[7-9]/\\d{2}")) {
+                System.out.println(employee.getName());
+            }
+        }
+
+        System.out.println("All employees who own a Mercedes:");
+        for (Employee employee : employeeList) {
+            if (employee.getVehicle().getMake().equals("Mercedes")) {
+                System.out.println(employee.getName());
+            }
+        }
+
+        System.out.println("All employees whose vehicle is registered in Hai Chau district:");
+        for (Employee employee : employeeList) {
+            if (employee.getVehicle().getRegistration().matches("43-[BC]1.*")) {
+                System.out.println(employee.getName());
+            }
+        }
+
+        System.out.println("All employees whose email is invalid:");
+        for (Employee employee : employeeList) {
+            if (!employee.getEmail().matches("^(.+)@([A-Z,a-z]+)\\.([A-Z,a-z]+)")) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
 }
